4c214317d090bb4478a25a581db43b4501818c1c
szkolakowski <szymon.kolakowski@gmail.com>
2022-10-30 02:24:54 +0200

    inrange update

--- haskell/01_inrange.hs
+++ haskell/01_inrange.hs
@@ -4,10 +4,14 @@ in_range :: Integer -> Integer -> Integer -> Bool
 
 -- in_range min max x =
 --     let in_lower_bound = min <= x
---         in_upper_bound = x <= max
+--         in_upper_bound = max >= x
 --     in 
 --         in_lower_bound && in_upper_bound
 
-in_range min max x = ilb && iub where
+-- in_range min max x = ilb && iub where
+--     ilb = min <= x
+--     iub = max >= x
+
+in_range min max x = if ilb then iub else False where
     ilb = min <= x
     iub = max >= x
