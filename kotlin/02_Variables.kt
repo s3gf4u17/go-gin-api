c7c29ab78e17c10fb357424652ec8e31664340cd
szkolakowski <szymon.kolakowski@gmail.com>
2022-10-31 01:00:04 +0100

    modified 02

---
+++ kotlin/02_Variables.kt
@@ -0,0 +1,11 @@
+fun main () {
+    sayHelloKotlin()
+
+    var username : String = "Miguel"
+    username = "Mikhail"
+    println("Current active user: $username")
+}
+
+fun sayHelloKotlin () {
+    println("Hello Kotlin!")
+}
