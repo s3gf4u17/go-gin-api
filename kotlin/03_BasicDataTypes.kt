60b0dee35f701ba5feac419250a63197b5b7866b
szkolakowski <szymon.kolakowski@gmail.com>
2022-10-31 01:26:22 +0100

    added 03

---
+++ kotlin/03_BasicDataTypes.kt
@@ -0,0 +1,9 @@
+fun main () {
+    val exampleString : String = "Florian"
+    val exampleInt : Int = 12
+    val exampleDouble : Double = 2.8
+    val exampleLongFromInt : Long = exampleInt.toLong()
+    val exampleChar : Char = '?'
+    val exampleIntFromChar = exampleChar.toInt()
+    var exampleBoolean : Boolean = true
+}
