f30b9ae8dd7618e639691999d60ec1162e877ea9
szkolakowski <szymon.kolakowski@gmail.com>
2022-10-31 01:06:49 +0100

    modified 02

--- kotlin/02_Variables.kt
+++ kotlin/02_Variables.kt
@@ -1,9 +1,11 @@
 fun main () {
     sayHelloKotlin()
 
-    var username : String = "Miguel"
-    username = "Mikhail"
-    println("Current active user: $username")
+    // val - locked value, cannot be reassigned
+    val username : String = "Miguel"
+    //username = "Mikhail"
+    val age : Int = 23
+    println("Current active user: $username, age: $age. In 2 years he will be ${age+2}.")
 }
 
 fun sayHelloKotlin () {
