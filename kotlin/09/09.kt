4d9ad525f0b5da2052b82a0ac9ca0261ca4305b9
szkolakowski <szymon.kolakowski@gmail.com>
2022-11-01 14:08:34 +0100

    if else conditions

---
+++ kotlin/09/09.kt
@@ -0,0 +1,9 @@
+// IF / ELSE
+fun main() {
+    var calories = 1900
+    if (calories>=2000){
+        println("You cant consume any more calories today!")
+    }else{
+        println("You can still consume ${2000-calories} calories today.")
+    }
+}
