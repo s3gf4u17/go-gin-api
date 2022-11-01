2d1b4241e8e468533927ed0fef5ae1044193a7d3
szkolakowski <szymon.kolakowski@gmail.com>
2022-11-01 20:28:46 +0100

    create functions

---
+++ kotlin/17/17.kt
@@ -0,0 +1,15 @@
+// create function
+fun main(){
+    helloWorld()
+    moo()
+    helloWorld()
+}
+
+fun helloWorld(){
+    println("Hello world!")
+    moo()
+}
+
+fun moo(){
+    println("moo")
+}
