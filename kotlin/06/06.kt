dd0fce8b56e7b7ab46ad0f4283692001319f39c9
szkolakowski <szymon.kolakowski@gmail.com>
2022-11-01 13:43:53 +0100

    string and char

--- kotlin/06/06.kt
+++ kotlin/06/06.kt
@@ -1,1 +1,13 @@
 // STRING AND CHAR
+fun main() {
+    val fullName : String = "John Felker"
+    println(fullName.length)
+    println(fullName.decapitalize())
+    println(fullName.toLowerCase())
+    println(fullName.toUpperCase())
+
+    val ascii : Char = 'x' // 16 bit unicode character
+    val tab : Char = '\t'
+    // val emoji : Char = '\u122'
+    println("$ascii $tab")
+}
