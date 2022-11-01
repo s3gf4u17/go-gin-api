5fd2f634230d835547cef76211b64035ea3e35b6
szkolakowski <szymon.kolakowski@gmail.com>
2022-11-01 14:24:43 +0100

    structural equality

---
+++ kotlin/12/12.kt
@@ -0,0 +1,11 @@
+// STRUCTURAL EQUALITY
+fun main(){
+    val name1 = "Bob"
+    val name2 = "Tushar"
+    val name3 = "Bob"
+
+    println(name1==name2)
+    println(name1.equals(name2))
+    println(name1==name3)
+    println(name1.equals(name3))
+}
