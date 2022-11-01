91b02c9dac29f3844b452436eab33dbe07ca5b35
szkolakowski <szymon.kolakowski@gmail.com>
2022-11-01 14:14:04 +0100

    single line if else statement

---
+++ kotlin/10/10.kt
@@ -0,0 +1,6 @@
+// SINGLE LINE IF ELSE STATEMENT
+fun main() {
+    val age = 17
+    val information = if (age > 18) "your age is greater than 18" else if (age==18) "your age is 18" else "your age is less than 18"
+    println(information)
+}
