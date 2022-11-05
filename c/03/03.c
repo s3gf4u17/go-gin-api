87ff6c269e18b649c81ff4aaa24e8ba3d9c1ccf9
szkolakowski <szymon.kolakowski@gmail.com>
2022-11-06 00:23:08 +0100

    printf

---
+++ c/03/03.c
@@ -0,0 +1,13 @@
+// printf
+#include<stdio.h>
+
+// placeholders begin with %:
+// %i - integer
+// more here https://cplusplus.com/reference/cstdio/printf/
+
+int main(){
+	printf("printf()");
+	printf("%i",0);
+	printf("%i%i",0,1);
+	return 0;
+}
