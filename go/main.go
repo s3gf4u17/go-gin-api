4d5f150ec571074631a2cbcffd28299dd4c6bbed
szkolakowski <szymon.kolakowski@gmail.com>
2022-12-07 22:41:26 +0100

    variables and data types

--- go/main.go
+++ go/main.go
@@ -3,36 +3,40 @@ package main
 import "fmt"
 
 func main() {
+	// HELLO WORLD
 	// fmt.Println("Hello World!")
 
+	// VARIABLES && DATA TYPES
+	var v1 string = "Hello"
+	fmt.Println(v1)
+
 	// strings
-	var nameOne string = "bob"
-	var nameTwo = "Bobbinson"
-	var nameThree string
-	fmt.Println(nameOne, nameTwo, nameThree)
-	nameThree = nameOne
-	nameOne = "rob"
-	fmt.Println(nameOne, nameTwo, nameThree)
+	// var nameOne string = "bob"
+	// var nameTwo = "Bobbinson"
+	// var nameThree string
+	// fmt.Println(nameOne, nameTwo, nameThree)
+	// nameThree = nameOne
+	// nameOne = "rob"
+	// fmt.Println(nameOne, nameTwo, nameThree)
+	// nameFour := "mob" // cannot be used outside a function
+	// fmt.Println(nameFour)
 
-	nameFour := "mob" // cannot be used outside a function
-	fmt.Println(nameFour)
+	// // ints
+	// var age1 int = 20
+	// var age2 = 30
+	// var age3 int
+	// age4 := 40
+	// fmt.Println(age1, age2, age3, age4)
 
-	// ints
-	var age1 int = 20
-	var age2 = 30
-	var age3 int
-	age4 := 40
-	fmt.Println(age1, age2, age3, age4)
+	// // bits and memory
+	// var num1 int8 = 25 // num16, num32, num64 also exist
+	// fmt.Println(num1)
+	// var num2 uint8 = 25 // unsigned
+	// fmt.Println(num2)
 
-	// bits and memory
-	var num1 int8 = 25 // num16, num32, num64 also exist
-	fmt.Println(num1)
-	var num2 uint8 = 25 // unsigned
-	fmt.Println(num2)
-
-	// floats
-	var score1 float32 = 1.0 // float32, float64
-	fmt.Println(score1)
-	score2 := 1.5 // default -> float64
-	fmt.Println(score2)
+	// // floats
+	// var score1 float32 = 1.0 // float32, float64
+	// fmt.Println(score1)
+	// score2 := 1.5 // default -> float64
+	// fmt.Println(score2)
 }
