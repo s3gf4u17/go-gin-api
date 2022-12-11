540fc2842d3caef116814d1c0d5f819ad0b7f14c
szkolakowski <szymon.kolakowski@gmail.com>
2022-12-11 02:27:50 +0100

    structs

--- go/other/main.go
+++ go/other/main.go
@@ -1,3 +1,4 @@
+// go mod init <folder-name>
 package main
 
 import (
@@ -17,4 +18,17 @@ func main() {
 	// fmt.Println(pointers.F1)
 	// fmt.Println(pointers.BasicTest())
 	// fmt.Println(*pointers.BasicTest())
+
+	var p1 structs.Point = structs.Point{1,2}
+	var p2 structs.Point = structs.Point{5,3}
+	fmt.Println(p1.X, p1.Y)
+	fmt.Println(p2.X, p2.Y)
+	p3 := p2 // creates a new, non connected "object"
+	p3.X = 7
+	fmt.Println(p2.X, p3.X)
+	p4 := structs.Point{X:3} // gives Y a default int 32 value
+	structs.ChangeX(&p4,999)
+	fmt.Println(p4.X, p4.Y)
+	c1 := structs.Circle{10,&structs.Point{0,0}}
+	fmt.Println(c1, c1.Center.X)
 }
--- go/other/structs/lib.go
+++ go/other/structs/lib.go
@@ -1,1 +1,16 @@
 package structs
+
+type Point struct {
+	X int32
+	Y int32
+}
+
+// embedded struct
+type Circle struct {
+	Radius float64
+	Center *Point // we dont even have to name it
+}
+
+func ChangeX(pt *Point, x int32){
+	(*pt).X = x
+}
