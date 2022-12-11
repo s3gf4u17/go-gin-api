72321c1d543c58f1586cc0738c2f1705fff11e2a
szkolakowski <szymon.kolakowski@gmail.com>
2022-12-11 02:01:22 +0100

    pointers

--- go/other/main.go
+++ go/other/main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"fmt"
-	"other/foo"
+	// "other/pointers"
+	"other/structs"
 )
 
 func main() {
-	fmt.Println(foo.F1)
-	fmt.Println(foo.GetLocation())
+	// fmt.Println(pointers.F1)
+	// fmt.Println(pointers.GetLocation())
+	// pointers.DirectModify(32)
+	// fmt.Println(pointers.F1)
+	// pointers.IndirectModify(-32)
+	// fmt.Println(pointers.F1)
+	// pointers.ChangeValue(&pointers.F1,32)
+	// fmt.Println(pointers.F1)
+	// fmt.Println(pointers.BasicTest())
+	// fmt.Println(*pointers.BasicTest())
 }
