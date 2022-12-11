2d91de66e78c01a3a947063902d26f4b9fbea85b
szkolakowski <szymon.kolakowski@gmail.com>
2022-12-11 23:46:25 +0100

    book data struct

--- go/main.go
+++ go/main.go
@@ -6,3 +6,24 @@ package main
 import "net/http"
 import "github.com/gin-gonic/gin"
 import "errors"
+
+// struct to store book data
+// capital case make variables available publicly for other modules
+// in json we want them to be lowercase
+type book struct{
+	ID string `json:"id"`
+	Title string `json:"title"`
+	Author string `json:"author"`
+	Quantity int `json:"quantity"`
+}
+
+// test list of books
+var books = []book{
+	{ID:"1",Title:"In search of Lost Time",Author:"Marcel Proust",Quantity:17},
+	{ID:"1",Title:"In search of Lost Time",Author:"Marcel Proust",Quantity:17},
+	{ID:"1",Title:"In search of Lost Time",Author:"Marcel Proust",Quantity:17}
+}
+
+func main() {
+
+}
