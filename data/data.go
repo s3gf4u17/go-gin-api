7e7ecde3648c4e02a3cd2501ece1aad746aaf8ba
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-01-05 14:05:35 +0100

    example books data

---
+++ data/data.go
@@ -0,0 +1,8 @@
+package data
+
+import "go-web-server/model"
+
+var Books = []model.Book{
+	model.Book{"tanzanian adventure","zack p morrison",32},
+	model.Book{"my big tuna friend","katy bomson",11},
+}
