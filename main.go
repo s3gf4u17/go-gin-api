88a50fa7b739d3607bc72fd3ba7cc5645ae8c918
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-01-07 14:50:49 +0100

    flag parsing

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "net/http"
+import "flag"
+import "fmt"
+
+var varHost = flag.String("host","127.0.0.1","")
+var varPort = flag.String("port","8000","")
+
+
+func main() {
+	flag.Parse()
+	URL:=fmt.Sprintf("%s:%s",*varHost,*varPort)
+	http.ListenAndServe(URL,nil)
+}
