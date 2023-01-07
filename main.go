e3bc4b683c2df6f4d351d6061b73341a589a856d
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-01-07 15:44:51 +0100

    serve http info

--- main.go
+++ main.go
@@ -7,10 +7,16 @@ import "fmt"
 
 var varHost = flag.String("host","127.0.0.1","")
 var varPort = flag.String("port","8000","")
+var varPath = flag.String("path","/","")
 
 
 func main() {
 	flag.Parse()
 	URL:=fmt.Sprintf("%s:%s",*varHost,*varPort)
+	http.HandleFunc("/",serveHTML)
 	http.ListenAndServe(URL,nil)
+}
+
+func serveHTML(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Gost requires some setup</h1><a href='https://github.com/s3gf4u17/gost/blob/master/README.md'>Learn more</a>"))
 }
